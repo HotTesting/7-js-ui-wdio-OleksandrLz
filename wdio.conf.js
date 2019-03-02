25ddfb4c197990a4c2ddfe65067b2b0a1c6199db
AleksandrLz <4oleksandrlz@gmail.com>
2019-03-02 10:58:36 +0200

    edit location for Registration test

--- wdio.conf.js
+++ wdio.conf.js
@@ -2,7 +2,7 @@ exports.config = {
   hostname: "localhost",
   port: 4444,
   path: "/wd/hub",
-  specs: ["./tests/registration.ts"],
+  specs: ["./tests/lesson3/registration.ts"],
   sync: true,
   services: ["selenium-standalone"],
   capabilities: [
